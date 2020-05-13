1a60c8f06ea139d9d8d457c31868ba582667a068
Perfumer <1061393710@qq.com>
2020-05-13 14:49:30 +0800

    修改配置-1

--- dev/vue.config.js
+++ dev/vue.config.js
@@ -9,9 +9,9 @@ module.exports = {
   configureWebpack: (config) => {
     if (process.env.NODE_ENV === 'production') {
       // 生产环境下的配置更改
-      config.externals = {
-        vue: 'Vue',
-      };
+      // config.externals = {
+      //   vue: 'Vue',
+      // };
     } else {
       // 开发环境下的配置更改
     }
