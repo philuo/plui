2dde8c780231e22e0edd054291aba5cebbf0b247
q788799660 <987343689@qq.com>
2020-05-18 20:45:53 +0800

    a

--- dev/src/Plui.vue
+++ dev/src/Plui.vue
@@ -1,36 +1,21 @@
 <template>
   <div id="app">
     <div class="flex btn_type">
-      <BaseButton msg="默认按钮" type="default"/>
-      <BaseButton msg="主要按钮" type="primary"/>
-      <BaseButton msg="危险按钮" type="danger"/>
-      <BaseButton msg="信息按钮" type="info"/>
-      <BaseButton msg="警告按钮" type="warning"/>
+      <pl-button msg="默认按钮" type="default"/>
+      <pl-button msg="主要按钮" type="primary"/>
+      <pl-button msg="危险按钮" type="danger"/>
+      <pl-button msg="信息按钮" type="info"/>
+      <pl-button msg="警告按钮" type="warning"/>
     </div>
-    <div class="flex btn_type">
-      <LoadButton loading  loading-type="" loading-text=""/>
-    </div>
+
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import BaseButton from './components/button/base-button.vue';
-import LoadButton from './components/button/load-button.vue';
-
-export default defineComponent({
-  components: {
-    BaseButton, LoadButton,
-  },
-  setup() {
-    return {
-
-    };
-  },
-});
-
 
 export default defineComponent({});
+
 </script>
 
 <style lang="scss">
