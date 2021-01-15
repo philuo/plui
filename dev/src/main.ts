8d990f0ee2711a8bbdc87382ad0d17ab90ec9bf9
Perfumere <1061393710@qq.com>
2021-01-15 15:16:39 +0800

    修改解释

--- dev/src/main.ts
+++ dev/src/main.ts
@@ -10,9 +10,10 @@ import App from './App.vue';
 // import '../build/style.css';
 
 // 发版后验证可用性使用
+// 不要在本地发版!!!
 // 需要发版前 `npm view @yuo/plui version` 看下最新的已存在的版本
 // 发版的tag 需要以小写字母v开头 *根据之前的版本来定，修改package.json中的version字段
-// 不要在本地发版，通过打tag方式由CI发版，执行 `git tag v*.*.*` 之后 `git push --tags`
+// 通过打tag方式由CI发版，执行 `git tag v*.*.*` 之后 `git push --tags`
 // @ts-ignore 因为没有写@types
 import Plui from '@yuo/plui';
 import '/@pkg/build/style.css';
