56e151ada07083367159fad2aa06b326fca0ef76
Perfumere <1061393710@qq.com>
2021-01-15 15:12:36 +0800

    新增解释

--- dev/src/main.ts
+++ dev/src/main.ts
@@ -1,7 +1,18 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-// import Plogui from './components';
 
+// 本地开发dev时使用
+// import Plui from './components';
+
+// 验证build产物使用
+// @ts-ignore
+// import Plui from '../build/@yuo/plui.es.js';
+// import '../build/style.css';
+
+// 发版后验证可用性使用
+// 需要发版前 `npm view @yuo/plui version` 看下最新的已存在的版本
+// 发版的tag 需要以小写字母v开头 *根据之前的版本来定，修改package.json中的version字段
+// 不要在本地发版，通过打tag方式由CI发版，执行 `git tag v*.*.*` 之后 `git push --tags`
 // @ts-ignore 因为没有写@types
 import Plui from '@yuo/plui';
 import '/@pkg/build/style.css';
